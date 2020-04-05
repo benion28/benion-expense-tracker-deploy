9e666216272dbb5c10fae5bda7d0f11ef519c8c1
Bernard Iorver <bernard.benion.iorver@hotmail.com>
2020-04-05 03:36:37 +0100

    Updates

---
+++ precache-manifest.e2b11ca773eb76bb96e656092dd00a09.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eda776fb07e31eecd4dc1b40fd5611d5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1c8333e5e0082a4af81b",
+    "url": "/static/css/main.d0e32a8e.chunk.css"
+  },
+  {
+    "revision": "a6365a6ee6d9b7bd5f56",
+    "url": "/static/js/2.51c2a764.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.51c2a764.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1c8333e5e0082a4af81b",
+    "url": "/static/js/main.d634fe7f.chunk.js"
+  },
+  {
+    "revision": "e32411b2549685f023a0",
+    "url": "/static/js/runtime-main.6ca5cbb2.js"
+  }
+]);
